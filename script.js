e47b27d4503e3a6458db904d2731bcaf86089a60
Aramayis <mayissargsyan1995@gmail.com>
2021-06-30 14:06:56 +0400

    fix:all bugs fixed

--- script.js
+++ script.js
@@ -1,38 +1,47 @@
-const search = document.querySelector(".input")
-const button = document.querySelector(".btn")
-button.addEventListener("submit", (e)=>{
-    e.preventDefault()
-})
-button.addEventListener("click", ()=>{
-    fetch(`https://restcountries.eu/rest/v2/name/${}`)
-  .then(response => response.json())
-  .then( response => response.filter(
-      (el) => {
-        el.includes(search.value)
-      }
-  ));  
-})
+const search = document.querySelector(".input");
+const button = document.querySelector(".btn");
+const body = document.querySelector(".body");
+const defined = document.querySelector(".not-found");
 
-function fetching(str){
-    fetch(`https://restcountries.eu/rest/v2/name/${str}`)
-    .then(response => response.json())
-    .then(data)
+var insertedContent = document.querySelector(".insertedContent");
+button.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+button.addEventListener("click", () => {
+  fetch(`https://restcountries.eu/rest/v2/name/${search.value}`)
+    .then((response) => response.json())
+    .then((response) => render(response));
+});
+
+function fetching(str) {
+  fetch(`https://restcountries.eu/rest/v2/name/${str}`)
+    .then((response) => response.json())
+    .then(data);
 }
 
-function render(data){
-data.forEach(el => {
+function render(data) {
+  const cards = document.querySelectorAll(".card");
+
+  if (cards.length) {
+    cards.forEach((card) => body.removeChild(card));
+  }
+  if (!data.length) {
+    defined.style.display = "block";
+  } else defined.style.display = "none";
+  data.forEach((el) => {
     const html = `
-            <div class="body"> 
                  <div class="card">
-                    <img src="${el.flag}">
+                    <img class="img" src="${el.flag}">
                     <h3>Name - ${el.name}</h3>
                     <h4>Capital - ${el.capital}</h4>
-                    <h4>Population - ${el.population}</h4>
+                    <h4>Population - ${(el.population / 1000000).toFixed(
+                      3
+                    )} mln</h4>
                     <h4>Area - ${el.area}</h4>
                     <h4>Region - ${el.region}</h4>
                 </div>
-            </div>
     `;
-body.insertAdjacentHTML('beforend', html);
-});
-};
+
+    body.insertAdjacentHTML("beforeEnd", html);
+  });
+}
